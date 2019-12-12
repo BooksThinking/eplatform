0fb3c592d62717099508950ef3cb54a470c7226f
LiaoRongHua <1245586921@qq.com>
2019-12-12 15:45:26 +0800

    ftrst of liao

---
+++ src/servlet/loginServlet.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class loginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader bufferedReader = req.getReader();
+        System.out.print(bufferedReader.readLine());
+    }
+
+}
