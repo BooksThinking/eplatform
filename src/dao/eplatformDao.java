26b561867239ac367a4b680d50d221276bbc3f5e
LiaoRongHua <1245586921@qq.com>
2019-12-14 00:05:15 +0800

    not many

--- src/dao/eplatformDao.java
+++ src/dao/eplatformDao.java
@@ -1,5 +1,37 @@
 package dao;
 
-public class eplatformDao {
+import entities.production;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class eplatformDao {
+    public List<production> getAllProduction(){
+        List<production> productionList = new ArrayList<>();
+        String driverClassName = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/eplatform";
+        String userName = "root";
+        String userPassword = "1245586921";
+        try {
+            Class.forName(driverClassName);
+            Connection connection = DriverManager.getConnection(url, userName, userPassword);
+            Statement statement = connection.createStatement();
+            String sql = "select * from production";
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                production a = new production();
+                a.setProductionName(rs.getString("productionName"));
+                a.setProductionPrice(rs.getString("productionPrice"));
+                a.setProductionNumber(rs.getString("productionNumber"));
+                productionList.add(a);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productionList;
+    }
 }
--- src/servlet/loginServlet.java
+++ src/servlet/loginServlet.java
@@ -13,8 +13,6 @@ import dao.loginDao;
 public class loginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        System.out.println("用户名称"+req.getParameter("userName"));
-//        System.out.println("用户密码"+req.getParameter("userPassword"));
         String userName = req.getParameter("userName");
         String userPassword = req.getParameter("userPassword");
         user requestUser = new user(userName,userPassword);
@@ -22,7 +20,7 @@ public class loginServlet extends HttpServlet {
         if (loginDao.checkUser(requestUser)){
             //如果存在这个用户我们能够跳转到电商平台页面
             resp.setContentType("text/html; charset=UTF-8");
-            resp.getWriter().write("用户名正确");
+            resp.sendRedirect("/loginSuccess.html");
         }else{
             //如果用户不存在我们就注册或者是重新输入密码和用户名
             resp.getWriter().write("你这个用户名错误");
